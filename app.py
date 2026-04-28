49e87aa00f58e64b195404909412e1e070cbcf59
Nurazizahra <nurazizahrabiulakhir@gmail.com>
2026-04-29 02:17:48 +0700

    test minimal app fix

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,3 +9,7 @@ def index():
 @app.route("/ping")
 def ping():
     return "OK"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
